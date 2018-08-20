7de203045ae6a7cfc80a0848d715e8f3fa0d64df
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-08-20 17:52:44 -0400

    up

--- residenceDuCalmeJ2EE/build/web/Scripts/ScriptsJS.js
+++ residenceDuCalmeJ2EE/build/web/Scripts/ScriptsJS.js
@@ -132,3 +132,15 @@ function MettreType(x) {
 
 
 }
+
+function mettrePrix() {
+
+    var textbox = document.getElementById("prix");
+
+    document.getElementById("div_prix").innerHTML =   
+  " <div data-role='rangeslider'><input type='range' name='price-min' id='price-min' value='200' min='0' max='1000'> <input type='range' name='price-max' id='price-max' value='800' min='0' max='10000'> </div> <div> <input type='submit' data-inline='true' value='Submit'></div>";
+
+
+
+
+}
